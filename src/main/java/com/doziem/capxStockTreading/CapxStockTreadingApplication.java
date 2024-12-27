ed78a29b98e43161498d92996c566920fc68e0e2
Kenneth Chidozie <agbokennethchidoziem@gmail.com>
2024-12-27 05:24:33 -0800

    capx project set up

---
+++ src/main/java/com/doziem/capxStockTreading/CapxStockTreadingApplication.java
@@ -0,0 +1,13 @@
+package com.doziem.capxStockTreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapxStockTreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapxStockTreadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doziem/capxStockTreading/CapxStockTreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doziem.capxStockTreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapxStockTreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
